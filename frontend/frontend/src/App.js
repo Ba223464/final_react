308d187cf0a423be4261556f97c22dc46a6cc739
Ba223464 <bala250820042gmail.com>
2025-03-29 17:42:01 +0000

    Added my folder

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,11 @@
+import PatientEHR from './patientehr';
+import HospitalEHR from './HospitalEHR';
+function App() {
+  return (
+    <div className="App">
+      <HospitalEHR />
+    </div>
+  );
+}
+
+export default App;
